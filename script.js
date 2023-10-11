5cea48c92c15dd08d8a8123f17c91fbb7af2e05b
JuniNam <147520685+JuniNam@users.noreply.github.com>
2023-10-11 14:43:52 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here 
 
 
 const loadDataFromLocalstorage = () => {
